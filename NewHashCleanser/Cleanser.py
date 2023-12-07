7aff5819d2cb5a67f3ab55d27d8a9d53e557d152
Landan <landan.hand@gmail.com>
2023-12-07 09:46:45 -0600

    modified:   NewHashCleanser/Cleanser.py 	modified:   NewHashCleanser/HashStuff/AbstractHashClass.py

--- NewHashCleanser/Cleanser.py
+++ NewHashCleanser/Cleanser.py
@@ -3,5 +3,20 @@ from HashStuff.DCC2 import DCC2
 from HashStuff.NTLM import NTLM
 import User
 
-something = DCC2()
-something.Parse()
+something = DCC2() # test
+something.Parse() # test
+
+    #FUNCTIONS WITHIN THE ORIGIONALPROGRAM#
+    # Main
+    # UserListToFile
+    # Matching
+    # Match_Parser
+    # Pattern_Parser
+    # Remove_Null
+    # Match
+    # Directory_Parser
+    # List_Cleanser
+    # Secrets_Parser
+    # File_Cleanser
+    # Rem_Duplicates
+    # List_To_File
--- NewHashCleanser/HashStuff/AbstractHashClass.py
+++ NewHashCleanser/HashStuff/AbstractHashClass.py
@@ -4,19 +4,66 @@ class Hash(ABC):
     
     hashes = []
     pattern = ""
+    pattern_HASH = r"(.*?):(.*)(.*):(.*):::"        # need to capture groups 1 for the username and 4 for the hash
+    pattern_CRACKED= r"(.*?):(.*)"                  # for use with the cracked password file group 1 is the hash and group 2 is the username
 
+
+    ### These functions are for all of the parsing operations ###
     @abstractmethod
-    def Parse(): #will need the secretsdump file or directory as well as the hash type
+    def Pattern_Parse(): #will need the secretsdump file or directory as well as the hash type
         # This function uses regular expression to fing NTLM or DCC2 hashes and returns the hashes in a list
         pass
 
+    @abstractmethod
+    def Match_Parser(): #will need a list of the 
+        pass
+
+    @abstractmethod
+    def Directory_Parser():
+        pass
+
+    @abstractmethod
+    def Secrets_Parser():
+        pass
+
+    ### All of these functions are for the matching operations ###
+    @abstractmethod
     def Match(): #will need the list of cracked hashes and the list of uncracked hashes
         # Function to grab the hash username and password information and return lists containing that information
         pass
 
-    def RemoveUnwantedChars(): #Will need the list of hashes
+    @abstractmethod
+    def Match_Helper():
+        pass
+
+    ### All of these functions are for the cleaning of data ###
+    @abstractmethod
+    def Remove_Unwanted_Chars(): #Will need the list of hashes
         # This function removes the null byte at the end of an item in a list
         pass
+
+    @abstractmethod
+    def File_Cleanser():
+        pass
+
+    @abstractmethod
+    def List_Cleanser():
+        pass
+
+    @abstractmethod
+    def Remove_Duplicates():
+        pass
+
+    ### These functions are for all of the data to file operations ###
+    @abstractmethod
+    def List_To_File():
+        pass
+    
+    @abstractmethod
+    def User_List_To_File():
+        pass
+    
+
 # For the regex for the matching function
 #   Hashes
 #       group 1: username
@@ -25,3 +72,18 @@ class Hash(ABC):
 #   cracked
 #       group 1: hash
 #       group 2: password
+
+#FUNCTIONS WITHIN THE ORIGIONAL PROGRAM#
+# Main ---> Cleanser.py
+# UserListToFile ---> User_List_To_File
+# Matching ---> Match_Helper
+# Match_Parser ---> Match_Parser
+# Pattern_Parser ---> Pattern_Parse
+# Remove_Null ---> Remove_Unwanted_Chars
+# Match ---> Match
+# Directory_Parser ---> Directory_Parser
+# List_Cleanser ---> List_cleanser
+# Secrets_Parser ---> Secrets_Parser
+# File_Cleanser ---> File_Cleanser
+# Rem_Duplicates ---> Remove_Duplicates
+# List_To_File ---> List_To_File
